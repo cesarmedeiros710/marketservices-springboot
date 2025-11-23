a98e125718c516f7537101c7183a297413526364
cesarmedeiros710 <cesarmedeiros768@gmail.com>
2025-11-23 15:02:30 -0300

    ajuste agendamento

--- src/main/java/dev/javaservice/marketservices/repository/AgendamentoRepository.java
+++ src/main/java/dev/javaservice/marketservices/repository/AgendamentoRepository.java
@@ -1,25 +1,7 @@
-package com.marketservices.service;
+package com.marketservices.repository;
 
 import com.marketservices.model.Agendamento;
-import com.marketservices.repository.AgendamentoRepository;
-import org.springframework.stereotype.Service;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
-@Service
-public class AgendamentoService {
-
-    private final AgendamentoRepository repo;
-
-    public AgendamentoService(AgendamentoRepository repo) {
-        this.repo = repo;
-    }
-
-    public List<Agendamento> findAll() {
-        return repo.findAll();
-    }
-
-    public Agendamento save(Agendamento agendamento) {
-        return repo.save(agendamento);
-    }
+public interface AgendamentoRepository extends JpaRepository<Agendamento, Long> {
 }
